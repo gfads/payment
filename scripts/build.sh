bd33134ef6321dddf2af1ee1b6c43e9dbbf9272a
Matheus Santana <embs@cin.ufpe.br>
2018-05-02 13:28:01 -0300

    Use rbinder

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,11 +14,11 @@ if [[ -z "$COMMIT" ]] ; then
     exit 1
 fi
 
-if [[ "$(uname)" == "Darwin" ]]; then
+#if [[ "$(uname)" == "Darwin" ]]; then
     DOCKER_CMD=docker
-else
-    DOCKER_CMD="sudo docker"
-fi
+#else
+#    DOCKER_CMD="sudo docker"
+#fi
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 
